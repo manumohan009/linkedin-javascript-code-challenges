6b4b6c30bc14dd9f1c6ac935397cac82190489ad
Manu Mohanan <manu.m@tataelxsi.co.in>
2022-01-09 00:12:21 +0530

    Completed  challenge 2: Movie object (class and prototypal inheritance)

---
+++ movie-object/index.js
@@ -0,0 +1,36 @@
+// function Movie(title, director, genre, releaseYear, rating){
+//     this.title = title;
+//     this.director = director;
+//     this.genre = genre;
+//     this.releaseYear = releaseYear;
+//     this.rating = rating;
+// }
+
+// Movie.prototype.getOverview = function(){
+//     return `${this.title}, a film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
+// }
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+  getOverview() {
+    return `${this.title}, a film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
+  }
+}
+
+const Spiderman = new Movie("Spiderman", "Sam Raini", "Action", 2002, 87);
+const Batman = new Movie(
+  "The Dark Knight",
+  "Christopher Nolan",
+  "Action",
+  2008,
+  83
+);
+
+console.log(Spiderman.getOverview());
+console.log(Batman.getOverview());
